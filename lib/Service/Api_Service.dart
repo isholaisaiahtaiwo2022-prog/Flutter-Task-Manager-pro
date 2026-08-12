5e888a38c62b077f607f3b01234133acdc7b83bd
isholaisaiahtaiwo2022-prog <isholaisaiahtaiwo2022@gmail.com>
2026-08-12 17:08:03 +0100

    implement  PUT,  and DELETE API requests

--- lib/Service/Api_Service.dart
+++ lib/Service/Api_Service.dart
@@ -41,4 +41,39 @@ class ApiService {
       throw Exception('Failed to create post');
     }
   }
+
+
+    // 3. PUT: Replace an existing post completely
+  Future<Task> updatePost(int id, String title, String body) async {
+    final response = await http.put(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts/$id'),
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
+      body: jsonEncode({
+        'id': id,
+        'title': title,
+        'body': body,
+        'userId': 1,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return Task.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to update post');
+    }
+  }
+
+  // 4. DELETE: Remove a post by ID
+  Future<bool> deletePost(int id) async {
+    final response = await http.delete(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts/$id'),
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
